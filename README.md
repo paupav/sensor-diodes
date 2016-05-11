7770bcf613d6d8e4c3cba52d254ce232a0e48879
Paulo Pavačić <pavacic.p@gmail.com>
2016-05-11 17:54:50 +0200

    added Classes

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # sensor-diodes
 thermometer with screen made from LED's
-## Warning, as I no longer own that chip I couldn't try this new and updated code.
-## For working code that is a bit longer check this link: ## https://github.com/paupav/sensor-diodes/blob/29262c26171f8b0607501ac1a90d9736d4a14e24/digitalTempSensor.ino
 
-Simply put this code on your arduino uno (or any other arduino) and connect everything to look like the scheme.
+Simply put this code on your arduino uno (or any other arduino) and connect everything as it is shown on  the scheme.
 
 ####Required equipment 
 - 38 LED's
--- digitalTempSensor.ino
+++ digitalTempSensor.ino
@@ -1,47 +1,51 @@
 short int temp;
 short int pinN[2][7];
 short int firstD, secondD;
-bool numbers[10];
 
+class Numbers
+{
+public:
+  bool states[7];
+};
+Numbers number[10];
 
 //makes it easier to set up numbers
-void addValues(bool &states[7], bool v1, bool v2, bool v3, bool v4, bool v5, bool v6, bool v7)
+void addValues(Numbers &numberl, bool v1, bool v2, bool v3, bool v4, bool v5, bool v6, bool v7)
 {
-   states[0] = v1;
-   states[1] = v2;
-   states[2] = v3;
-   states[3] = v4;
-   states[4] = v5;
-   states[5] = v6;
-   states[6] = v7;
+   numberl.states[0] = v1;
+   numberl.states[1] = v2;
+   numberl.states[2] = v3;
+   numberl.states[3] = v4;
+   numberl.states[4] = v5;
+   numberl.states[5] = v6;
+   numberl.states[6] = v7;
 }
 
 //gives array of premade signals for each number
 void createNumbers()
 {
-  addValues(numbers[0], 1,1,1,0,1,1,1);
-  addValues(numbers[1], 0,0,1,0,0,1,0);
-  addValues(numbers[2], 1,0,1,1,1,0,1);
-  addValues(numbers[3], 1,0,1,1,0,1,1);
-  addValues(numbers[4], 0,1,0,1,0,1,0);
-  addValues(numbers[5], 1,1,0,1,0,1,1);
-  addValues(numbers[6], 1,1,0,1,1,1,1);
-  addValues(numbers[7], 1,0,1,0,0,1,0);
-  addValues(numbers[8], 1,1,1,1,1,1,1);
-  addValues(numbers[9], 1,1,1,1,0,1,1);
+  addValues(number[0], 1,1,1,0,1,1,1);
+  addValues(number[1], 0,0,1,0,0,1,0);
+  addValues(number[2], 1,0,1,1,1,0,1);
+  addValues(number[3], 1,0,1,1,0,1,1);
+  addValues(number[4], 0,1,0,1,0,1,0);
+  addValues(number[5], 1,1,0,1,0,1,1);
+  addValues(number[6], 1,1,0,1,1,1,1);
+  addValues(number[7], 1,0,1,0,0,1,0);
+  addValues(number[8], 1,1,1,1,1,1,1);
+  addValues(number[9], 1,1,1,1,0,1,1);
 }
 
 //turns states into signals
-void chSt(bool digit, bool states[7])
+void chSt(bool digit, Numbers numberl)
 {
     
     for(int y = 0; y < 7; y++)
-      digitalWrite(pinN[digit][y],  states[y]);
+      digitalWrite(pinN[digit][y],  numberl.states[y]);
 }
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 13 as an output.
   pinMode(14, INPUT);
   for(int i = 0; i <= 13; i++)
     pinMode(i, OUTPUT);
